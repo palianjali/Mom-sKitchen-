821c6223e46c30e61acc539b99c2526571b913e1
palianjali <pali60658@gmail.com>
2025-05-16 12:04:25 +0530

    25

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -11,9 +11,9 @@ import img3 from './img3.png'
 import img4 from './img4.jpg'
 import img5 from './img5.jpg'
 import img6 from './img6.jpg'
-import img7 from './img6.jpg'
+import img7 from './img7.jpg'
 
-import img8 from './img6.jpg'
+import img8 from './img8.jpg'
 export const assets = {
     logo,
     logo2,
